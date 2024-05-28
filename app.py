21cae03dbfa55151e0c95aa918870eeff18a4c69
cdinsomnia <michele.chris.hempel@outlook.de>
2024-05-28 18:31:01 +0200

    Adjustments for production

--- app.py
+++ app.py
@@ -4,30 +4,24 @@ from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 import os
 
-# Laden der Umgebungsvariablen aus der .env-Datei
 load_dotenv()
 
-# Lesen der Umgebungsvariablen
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
 SIGNING_SECRET = os.getenv('SIGNING_SECRET')
 
-# Flask App initialisieren
 app = Flask(__name__)
 
-# Slack Client und Event Adapter initialisieren
 slack_client = WebClient(token=SLACK_TOKEN)
 slack_events_adapter = SlackEventAdapter(SIGNING_SECRET, "/slack/events", app)
 
-# Befehl "/hello" verarbeiten
 @slack_events_adapter.on("app_mention")
 def handle_hello_command(event_data):
     event = event_data["event"]
     if "Hello" in event.get('text', []):
         channel = event['channel']
-        message = "Hallo! Ich bin bereit, dir zu helfen."
+        message = "Hello world! I am IN42. And a bot. I am just here for testing."
         slack_client.chat_postMessage(channel=channel, text=message)
 
-# Ereignis-Endpunkt für URL-Verifizierung hinzufügen
 @app.route('/slack/events', methods=['POST'])
 def slack_events():
     data = request.json
@@ -37,4 +31,4 @@ def slack_events():
     return '', 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, port=5000)
